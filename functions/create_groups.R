a385ea30f229a898cd7fdd5cff24c8946114b282
Onur Tuncay Bal <onurtuncaybal@gmail.com>
2023-03-07 04:45:55 +0300

    Fixed Bugs

--- functions/create_groups.R
+++ functions/create_groups.R
@@ -1,4 +1,6 @@
 create_groups <- function(paths){
+  ##For this function to work you have to divide the followers in to two groups as folders. function accepts the folder paths
+  ##and returns the followers from all followers list as a one group. After that you can use this on political stance function. 
 require(tidyverse)
 main.followers <- paths
     for(folder in 1:length(paths)){
@@ -7,11 +9,29 @@ main.followers <- paths
             for(file in 1:length(files)){
            temp.file <- readr::read_csv(paste(paths[[folder]],"/",files[file],sep = ""))
            temp.followers <- temp.file%>%select(c("from_id"))
-           
            main.followers[[folder]][file] <- temp.followers
         } 
     }
+  names(main.followers) <- paths
+  main.followers <- reshape2::melt(main.followers)
+  group.types <- unique(main.followers$L1)
+  for(type in 1:length(group.types)){
+    assign(paste("group_",type,sep = ""),main.followers%>%filter(main.followers$L1 %in% 
+    group.types[type]))
     
-  return(main.followers)
-##Check Melt from Reshape Package
+    temp <- eval(parse(text = paste("group_",type, sep = "")))
+    assign(paste("group_",type,sep = ""), temp[,1], envir = .GlobalEnv)
+    
+  }
 }
+
+
+
+
+path_1 <- "~/Desktop/test/followers/group_b"
+path_2 <- "~/Desktop/test/followers/group_a"
+paths <- list(path_1,path_2)
+
+create_groups(paths = paths)
+
+length(paths)
--- functions/political_stance.R
+++ functions/political_stance.R
@@ -1,19 +1,3 @@
-political_stance <- function(network, groups){
-    require(ggraph)
-    require(igraph)
-    require(tidyverse)
-    political_str <- as.data.frame(matrix(NA_character_, nrow = length(V(network)$name), ncol = length(groups) + 1))
-    political_str[,1] <- V(network)$name
-    for(group in 1:length(groups)){
-        
-        for(user in 1:nrow(political_str)){
-            political_str[user,group + 1] <- length(which(groups[[group]] == political_str[user,1]))
-        }
-        }
-       
-    return(political_str)       
+political_stance <- function(network,groups){
+    
 }
-  
-
-
-
