aac8024eb4d446add16de2f8ae706af2548c8275
Onur Tuncay Bal <94485797+welkiej7@users.noreply.github.com>
2023-05-25 14:02:58 +0300

    Update pagination_v2.R

--- functions/pagination_v2.R
+++ functions/pagination_v2.R
@@ -6,14 +6,7 @@ paginate_trends <- function(country = "Turkey", path = "~/networked/"){
 library(rtweet)
 library(tidyverse)
 
-access.secret <- "Nay96X3gG4EvcvkgbGwJ92oC3idKLrZPblHhGesjTgUOL" # nolint
-access.token <-  "1219650013502017543-ZGwvmEkTWDDlAqoUFef7Ts5Jbc7h3z" # nolint
-api.key <- "gy1HKM5XSsjd8DpqtoolTOWB1"#nolint
-api.secret <- "IvkdYhPaC7T35qrae8JbulNAAfJGTM2wPuW7KykaLjKkGxbJac"#nolint
-bearer <- "AAAAAAAAAAAAAAAAAAAAAJekdAEAAAAAyIu%2BWh3mjEhcSeYo1oNNGKE5A%2B0%3Dm83VXFkQskCDBmu3jiRTbzimW3VtoYafPiOEPDyVSETXDRY5ND"
-app.name <- "Turkey_Misinformation"
-create_token(app = app.name, consumer_key = api.key, consumer_secret = api.secret,
-             access_token = access.token, access_secret = access.secret) -> token.auth
+
 
 
 
